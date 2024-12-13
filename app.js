9e9be058be4a5260c9d6f131a370f1cbe8e5b386
Avishkar <chavanavishkar98&gmail.com>
2024-12-13 18:01:32 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,93 @@
+const express= require("express");
+const app= express();
+const mongoose = require("mongoose");
+const path= require("path");
+const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust";
+const methodOverride= require("method-override");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema} = require("./schema.js"); 
+const Review = require("./models/review.js");
+const listings= require("./routes/listing.js");
+const reviews= require("./routes/review.js");
+
+
+
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+
+
+
+
+main()
+.then(()=>{
+    console.log("connected to db");
+})
+.catch((err)=>{
+    console.log(err);
+})
+async function main() {
+
+    await mongoose.connect(MONGO_URL);
+  
+  
+}
+
+
+app.get("/",(req, res)=>{
+    res.send("hi, I am root");
+});
+
+
+
+
+app.get("/",(req, res)=>{
+    res.send("hi, I am root");
+});
+
+
+
+
+
+
+
+
+
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.all("*",(req, res, next)=>{
+    next(new ExpressError(404,"page not found !!"));
+})
+
+
+
+app.use((err, req, res, next)=>{
+    let {status=500, message="something went wrong"}= err;
+    res.status(status).render("error.ejs",{message});
+   // res.status(status).send(message);
+})
+
+
+
+app.listen(8080,()=>{
+    console.log("server is listening to port 8080");
+});
